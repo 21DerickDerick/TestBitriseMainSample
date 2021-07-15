826b86c35571a82b103c03946b881d6ae7f01bd1
Derick <Derick.Derick@rakuten.com>
2021-07-15 15:59:45 +0800

    Init main app

---
+++ MainApp/MainApp/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by Derick, Derick | RASIA on 18/6/21.
+//
+
+import UIKit
+//import FirstModule
+
+class ViewController: UIViewController {
+
+    //let test = MainModule()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+    }
+
+    @IBAction func didTapButton(_ sender: UIButton) {
+        //test.getFirstScreen()
+    }
+    
+    @IBAction func didTapButton2(_ sender: UIButton) {
+        //test.getSecondScreen()
+    }
+}
+
